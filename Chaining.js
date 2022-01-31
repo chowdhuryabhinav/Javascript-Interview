cd39f99e02dd05d693e219caacc1bb7db0c93c4f
Chowdhury <abhinav.chowdhury@capgemini.com>
2022-01-31 13:34:26 +0530

    Adding Chaining.js file

---
+++ Chaining.js
@@ -0,0 +1,22 @@
+const sailors = [
+  {
+    name: "yi hong",
+    active: true,
+    email: "yh@yhproductions.io",
+  },
+  {
+    name: "alex",
+    active: true,
+    email: "",
+  },
+  {
+    name: "nathan",
+    active: false,
+    email: "",
+  },
+];
+
+sailors
+  .filter((sailor) => sailor.active)
+  .map((sailor) => sailor.email || `${sailor.name}@wiscsail.io`)
+  .forEach((sailor) => console.log(sailor));
